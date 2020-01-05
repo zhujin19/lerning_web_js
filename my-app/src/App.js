3338d66b705dce4b2eb300ffb99d0970c2b6d6f7
zhujin19@live.com <zhujin19@Live.com>
2020-01-05 23:45:47 +0800

    利用b站视频搭建react脚手架

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          hello world, this is from jzhu1 20200104
+        </p>
+        <a
+          className="App-link"
+          href="https:www.baidu.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          百度一下你就知道
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
